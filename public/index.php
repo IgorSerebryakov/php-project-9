b95ca4b5de4ffb8e93efb543ad0d6ee6aac5366c
IgorSerebryakov <serebryakov09@mail.ru>
2024-05-16 23:18:39 +0300

    created db's

--- public/index.php
+++ public/index.php
@@ -3,16 +3,29 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use PostgreSQL\Connection;
-
-
+use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+use DI\Container;
 
 try {
     Connection::get()->connect();
-    echo 'A connection to db has been established successfully.';
 } catch (\PDOException $e) {
     echo $e->getMessage();
 }
 
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+$app = AppFactory::createFromContainer($container);
+
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
 
 
 
