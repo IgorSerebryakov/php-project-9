bc256e0f728be8b345271a2cd0299fd6feb5fa93
IgorSerebryakov <serebryakov09@mail.ru>
2024-05-19 13:36:50 +0300

    added OOP + insert into db

---
+++ app/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PostgreSQL;
+
+class Database
+{
+    private $pdo;
+    
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+    public function insertUrl($name, $created_at)
+    {
+        $sql = 'INSERT INTO urls (name, created_at) VALUES (:name, :created_at)';
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            'name' => $name,
+            'created_at' => $created_at
+        ]);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,32 +3,56 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use PostgreSQL\Connection;
+use PostgreSQL\Database;
 use Slim\Factory\AppFactory;
 use Slim\Middleware\MethodOverrideMiddleware;
 use DI\Container;
 
+session_start();
+
 try {
-    Connection::get()->connect();
+    $pdo = Connection::get()->connect();
 } catch (\PDOException $e) {
     echo $e->getMessage();
 }
+
+date_default_timezone_set("Europe/Moscow");
 
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+
 $container->set('flash', function () {
     return new \Slim\Flash\Messages();
 });
-$app = AppFactory::createFromContainer($container);
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
 
 $router = $app->getRouteCollector()->getRouteParser();
 
 $app->addErrorMiddleware(true, true, true);
 $app->add(MethodOverrideMiddleware::class);
 
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');    
+});
 
+$app->post('/urls', function ($request, $response) use ($pdo) {
+    $url = $request->getParsedBodyParam('url');
+    $dateOfCreation = date("Y-m-d H:i:s");
+    
+    $db = new Database($pdo);
+    $db->insertUrl($url['name'], $dateOfCreation);
+    
+    $html = var_export($url['name'], true);
+    $response->getBody()->write($html);
+    
+    return $response;
+});
 
+$app->run();
 
 
 
