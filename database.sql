e6d9185ba7ce1d70b47889615b222c921a6fa724
IgorSerebryakov <serebryakov09@mail.ru>
2024-05-22 20:54:13 +0300

    add checks db

--- database.sql
+++ database.sql
@@ -1,6 +1,16 @@
 DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS checks;
 CREATE TABLE urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP
+);
+CREATE TABLE url_checks (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE,
+    status_code SMALLINT,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
     created_at TIMESTAMP
 );
